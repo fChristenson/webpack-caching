d625cedd1960e079aea8bd57c11ef52bbb5da83e
Fredrik <fredrik.christenson@gmail.com>
2017-11-15 20:42:48 +0100

    cache

--- src/server.js
+++ src/server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "..", "dist")));
+app.use(
+  express.static(path.resolve(__dirname, "..", "dist"), {
+    maxAge: "1y"
+  })
+);
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "views", "no_hash.html"));
